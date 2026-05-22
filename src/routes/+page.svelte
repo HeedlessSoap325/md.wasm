b891272693cec1cf1bf07a36f6e8c64f3c5bcb9a
M. Fritz <mathisjulian123@gmail.com>
2026-05-22 23:02:06 +0200

    feat: load wasm binding via svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
+<script lang="ts">
+	import { onMount } from "svelte"
+  	import init, { word_count } from "../wasm/lib";
+
+	onMount(() => {
+		init().then(() => {
+			console.log(word_count("Hello World!"))
+		})
+	})
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
